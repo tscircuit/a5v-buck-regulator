4d67ce69760e2432a072901f6fe5b5b3a3fbd76b
seveibar <seveibar@gmail.com>
2024-06-10 08:57:06 -0700

    wip

--- lib/MyCircuit.tsx
+++ lib/MyCircuit.tsx
@@ -1,12 +1,16 @@
 import { layout } from "tscircuit"
-  import manual_edits from "./MyCircuit.manual-edits"
+import manual_edits from "./MyCircuit.manual-edits"
 
-  export const MyCircuit = () => (
-    <group layout={layout().manualEdits(manual_edits)}>
-      <resistor
-        resistance="10kohm"
-        name="R1"
-        footprint="0805"
-      />
-    </group>
-  )
+export const MyCircuit = () => (
+  <group layout={layout().manualEdits(manual_edits)}>
+    <resistor resistance="10kohm" name="R1" footprint="0805" />
+    <resistor
+      resistance="10kohm"
+      name="R2"
+      footprint="0805"
+      pcbX={"10mm"}
+      pcbY={0}
+    />
+    <trace from=".R1 > .left" to=".R2 > .right" />
+  </group>
+)
