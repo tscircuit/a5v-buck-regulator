4dccc4f2a882313aa7f4f400f571a0dc176e1a32
seveibar <seveibar@gmail.com>
2024-06-12 11:55:53 -0700

    wip

--- lib/MyCircuit.tsx
+++ lib/MyCircuit.tsx
@@ -5,15 +5,6 @@ import AP63357 from "./AP63357"
 export const MyCircuit = () => {
   return (
     <group layout={layout().manualEdits(manual_edits)}>
-      {/* <resistor resistance="10kohm" name="R1" footprint="0805" />
-      <resistor
-        resistance="10kohm"
-        name="R2"
-        footprint="0805"
-        pcbX={"10mm"}
-        pcbY={0}
-      />
-      <trace from=".R1 > .left" to=".R2 > .right" /> */}
       <AP63357 name="U1" />
     </group>
   )
